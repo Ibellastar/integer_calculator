c3a251e2639029e8566f1d65bf6629cae5fbdbd2
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-02 15:06:48 -0500

    calcualtor files created

--- src/main.c
+++ src/main.c
@@ -1,9 +1,31 @@
-#include "multiplier.h"
 #include <stdio.h>
+#include "calculator.h"
 
 int main() {
-    int x = 5, y = 4;
-    int result = multiplier(x, y);
-    printf("Result: %d\n", result);
+    int num1, num2;
+    char op;
+
+    printf("Enter first integer: ");
+    scanf("%d", &num1);
+
+    printf("Enter operator (+ - * /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second integer: ");
+    scanf("%d", &num2);
+
+    switch (op) {
+        case '+': printf("Result: %d\n", add(num1, num2)); break;
+        case '-': printf("Result: %d\n", subtract(num1, num2)); break;
+        case '*': printf("Result: %d\n", multiply(num1, num2)); break;
+        case '/': 
+            if (num2 != 0)
+                printf("Result: %d\n", divide(num1, num2));
+            else
+                printf("Error: Division by zero.\n");
+            break;
+        default: printf("Invalid operator.\n");
+    }
+
     return 0;
 }
--- src/multiplier.h
+++
@@ -1,6 +0,0 @@
-#ifndef MULTIPLIER_H
-#define MULTIPLIER_H
-
-int multiplier(int a, int b);
-
-#endif
